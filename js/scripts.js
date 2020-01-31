d1805b032760e53eb1ef0c7db073ed18a747cbf2
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-31 10:56:26 -0800

    Prepend new pizza info to DOM order list upon clicking 'Add Za'

--- js/scripts.js
+++ js/scripts.js
@@ -20,18 +20,62 @@ Order.prototype.incrementId = function(pizza) {
   pizza.id = this.currentId;
 }
 
+Order.prototype.findPizza = function(id) {
+  if(this.pizzas[id - 1]) {
+    for(let i = 0; i < this.pizzas.length; i++) {
+      if(pizzas[i].id === id) {
+        return pizza[i];
+      }
+    }
+  }
+  return false;
+}
+
 /////////////////////////////////////////////
 // PIZZA OBJECT //
 //////////////////
-function Pizza(toppingArr, size) {
+function Pizza(toppingArr, size, price) {
   this.toppings = toppingArr;
   this.size = size;
+  this.price = price;
 }
+
 
 //===========================================
 //FRONT-END
 //-------------------------------------------
 
+//Order object
+
+//Pizza object
+Pizza.prototype.writePizza = function() {
+  var sizeImage;
+  switch(this.size) {
+    case "Mercury":
+      sizeImage = "img/mercury.jpg";
+      break;
+    case "Earth":
+      sizeImage = "img/earth.jpg";
+      break;
+    case "Mars":
+      sizeImage = "img/mars.jpg";
+      break;
+  }
+  var toppingString = "";
+  for(let i = 0; i < this.toppings.length; i++) {
+    if(i === this.toppings.length - 1) {
+      toppingString += this.toppings[i];
+    } else {
+      toppingString += (this.toppings[i] + ", ");
+    }
+  }
+  var htmlString = `<div class="pizza"><img src=${sizeImage} alt="Picture of pizza"><div class="pizzaInfo"><h5 id="topping">${toppingString}</h5><hr><p>Size: <span id="size">${this.size}</span></p><p>Cost: $<span id="cost">24</span></p></div><div class="buttons"><button id="deleteButton" class="btn btn-danger btn-sm">X</button><button id="editButton" class="btn btn-warning btn-block">EDIT</button></div></div>`;
+  $("div#pizzas").prepend(htmlString);
+}
+
+
+
+//On page load
 $(document).ready(function() {
   var order = new Order();
   var size;
@@ -51,8 +95,8 @@ $(document).ready(function() {
       })
       var pizza = new Pizza(toppingArr, size)
       order.addPizza(pizza);
-      console.log(pizza);
       //append pizza object to div#pizzas
+      pizza.writePizza();
     } else {
       alert("Please select a pizza size!");
     }
